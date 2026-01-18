a6e88e5853670139b872d80ea899d64dbb277500
Larry Chiem <larrypchiem@gmail.com>
2026-01-17 17:37:25 -0800

    tough types

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,16 +1,35 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
+/*  exercise files. I've already created the tsconfig.json file, but your challenge is to find a place to apply each of the techniques that I've shown you in this chapter. 
+First, create an interface to describe the structure of this variable. Hint: It's an array. So you want to define the interface for the type of items that are in the array, not the whole array.
+Next, I want you to strongly type some hard-coded values with an enum. Then, apply types to the parameters and return values of these functions. 
+And also, use a generic parameter to define the parameter type of this function. This one will probably take you a little bit longer to finish. You might be able to finish it in under 15 minutes, but don't be disappointed if it takes you longer than that.
+*/
+interface ToDo {
+    id: number;
+    title: string;
+    status: TodoStatus;
+    completedOn?: Date;
+}
+
+enum TodoStatus {
+    Done = "done",
+    InProgress = "in-progress",
+    ToDo = "todo"
+}
+
+const todoItems: ToDo[] = [
+    { id: 1, title: "Learn HTML", status: TodoStatus.Done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: TodoStatus.InProgress },
+    { id: 3, title: "Write the best app in the world", status: TodoStatus.ToDo },
 ]
 
-function addTodoItem(todo) {
+// And also, use a generic parameter to define the parameter type of this function. This one will probably take you a little bit longer to finish. You might be able to finish it in under 15 minutes, but don't be disappointed if it takes you longer than that.
+function addTodoItem(todo: string): ToDo {
     const id = getNextId(todoItems)
 
     const newTodo = {
         id,
         title: todo,
-        status: "todo",
+        status: TodoStatus.ToDo,
     }
 
     todoItems.push(newTodo)
@@ -18,7 +37,7 @@ function addTodoItem(todo) {
     return newTodo
 }
 
-function getNextId(items) {
+function getNextId<T extends { id: number }>(items: T[]): number {
     return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
 }
 

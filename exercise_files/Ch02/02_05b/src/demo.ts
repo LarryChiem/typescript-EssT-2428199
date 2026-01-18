30b4ab950a997c90e0b0d6dd8223d63048748a98
Larry Chiem <larrypchiem@gmail.com>
2026-01-17 17:16:29 -0800

    Generics

--- exercise_files/Ch02/02_05b/src/demo.ts
+++ exercise_files/Ch02/02_05b/src/demo.ts
@@ -1,8 +1,14 @@
 interface Contact {
     id: number;
     name: string;
+    clone(name: string): Contact
 }
 
-function clone(source) {
+// have clone accept a clone function as a parameter
+// this resembles a function signature
+function clone(source: Contact): Contact {
     return Object.apply({}, source);
 }
+
+const a: Contact = { id: 123, name: "larry chiem" };
+const b = clone(a)
--- exercise_files/Ch02/02_06b/src/demo.ts
+++ exercise_files/Ch02/02_06b/src/demo.ts
@@ -3,9 +3,25 @@ interface Contact {
     name: string;
 }
 
-function clone(source: Contact): Contact {
+// Generic <TExternalId>
+interface UserContact<TExternalId> {
+    id: number
+    name: string
+    username: string
+    // Generic syntax <TExternalId> on the interface. Now, you can refer to it from externalId
+    externalId: TExternalId 
+    loadExternalId(): Task<TExternalId>
+}
+
+// The return type matches the input type so we restrict The second parameter needs to match the first type parameter by using the extends
+function clone<T1, T2 extends T1>(source: T1): T2 {
     return Object.apply({}, source);
 }
 
+// Since Contact and UserContact share some simlar properties. 
 const a: Contact = { id: 123, name: "Homer Simpson" };
-const b = clone(a)
+const b = clone<Contact, UserContact>(a)
+
+// Definind a metatype using generics
+const dateRange = { startDate: Date.now(), endDate: Date.now() }
+const dateRangeCopy = clone(dateRange)

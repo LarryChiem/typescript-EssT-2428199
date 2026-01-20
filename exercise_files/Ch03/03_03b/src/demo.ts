d9a97b687e01c2a86033d9923350fb5c40eec095
Larry Chiem <larrypchiem@gmail.com>
2026-01-19 16:21:04 -0800

    typeof operator

--- exercise_files/Ch03/03_03b/src/demo.ts
+++ exercise_files/Ch03/03_03b/src/demo.ts
@@ -16,7 +16,9 @@ interface Contact {
     status?: ContactStatus;
 }
 
-function toContact(nameOrContact) {
+// JS dynamic typing system. nameOrContact is type any.
+// function toContact(nameOrContact) {
+function toContact(nameOrContact: string | Contact): Contact {
     if (typeof nameOrContact === "object") {
         return {
             id: nameOrContact.id,
@@ -32,3 +34,8 @@ function toContact(nameOrContact) {
         }
     }
 }
+
+// 
+const myType = { min: 1, max: 200 }
+
+function save(source: typeof myType) {}
